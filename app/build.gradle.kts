7320b6fda91fa1dac07f86c2ca65c450c4d91c76
Pranav Maganti <pranav.maganti@gmail.com>
2021-01-01 19:23:15 +0000

    Cleanup gradle files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,6 @@ android {
         versionCode = 1
         versionName = "1.0"
 
-        // testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         testInstrumentationRunner = "com.vanpra.amblor.ComposeInstrumentTestRunner"
 
         buildConfigField(
